9e4e496365ddd1b800605fe81ce23b4b0c5fc7e9
Kumar Yash Raj <kumaryashraj21@gmail.com>
2026-08-29 00:23:47 +0530

    Add Longest-Consecutive-Sequence (union-find)

---
+++ leetcode/union-find/128-Longest-Consecutive-Sequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // if(nums.empty()) return 0;
+        unordered_set<int>st;
+        int longest=0;
+        for(auto& it:nums){
+            st.insert(it);
+        }
+
+        for(auto& it:st){
+            if(st.find(it-1)==st.end()){
+                int k=it;
+                int count=0;
+                while(st.find(k)!=st.end()){
+                    count++;
+                    k++;
+                }
+                longest = max(longest,count);
+            }
+        }
+        return longest;
+    }
+};
