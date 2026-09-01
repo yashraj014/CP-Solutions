198a25e1213ffbb0ae070ffa55287b40b812ba23
Kumar Yash Raj <kumaryashraj21@gmail.com>
2026-09-01 20:00:45 +0530

    Add Move-Zeroes (two-pointers)

--- leetcode/two-pointers/283-Move-Zeroes.cpp
+++ leetcode/two-pointers/283-Move-Zeroes.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int lastZero =0;
-        int n  = nums.size();
+        int insertPos = 0;
+        int n = nums.size();
         for(int i=0;i<n;i++){
-            while(lastZero<n && nums[lastZero]!=0) lastZero++;
-
-            if(lastZero>=0 && lastZero<n){
-                if(nums[i]!=0 && lastZero<i){
-                    swap(nums[i],nums[lastZero]);
-                }
+            if(nums[i]!=0){
+                swap(nums[i],nums[insertPos]);
+                insertPos++;
             }
-
         }
     }
 };
