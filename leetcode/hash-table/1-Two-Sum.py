b56587a88a3a6aa0bec108cf7a52be231148e7ee
Kumar Yash Raj <kumaryashraj21@gmail.com>
2026-09-06 14:37:56 +0530

    Add Two-Sum (hash-table)

---
+++ leetcode/hash-table/1-Two-Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        d = {}
+
+        for i in range(len(nums)):
+            if (target - nums[i]) in d :
+                return [d[target-nums[i]],i]
+            
+            d[nums[i]]=i
+        return [-1,-1]
+
+        
