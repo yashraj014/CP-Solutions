9bc3a945d35f5fa56d9666867f6abc7d090ff2bd
Kumar Yash Raj <kumaryashraj21@gmail.com>
2026-09-06 14:52:43 +0530

    Add Valid-Anagram (hash-table)

--- leetcode/hash-table/242-Valid-Anagram.py
+++ leetcode/hash-table/242-Valid-Anagram.py
@@ -1,17 +1,7 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t) :
-            return False
-        mpp = {}
-
-        for i in range(len(s)):
-            mpp[s[i]]=mpp.get(s[i],0)+1
-            mpp[t[i]]=mpp.get(t[i],0)-1
-        
-        for val in mpp.values():
-            if val!=0:
-                return False
-        return True
+        return Counter(s)==Counter(t)
 
     
         
