f4e623edfa7dd7f213da7b4d73af5834b461f5e9
Kumar Yash Raj <kumaryashraj21@gmail.com>
2026-09-09 19:33:39 +0530

    Add Majority-Element (hash-table)

--- leetcode/hash-table/169-Majority-Element.java
+++ leetcode/hash-table/169-Majority-Element.java
@@ -1,24 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int num=nums[0];
+        int num=0;
         int sum=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==num){
-                sum++;
-            }
-            else
-            sum--;
-
-            if(sum==0){
-                num=nums[i];
-                sum++;
-            }
+           if(sum==0){
+            num=nums[i];
+           }
+           sum+=(num==nums[i])?1:-1;
         }
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==num) count++;
-        }
-        if(count>=nums.length/2) return num;
-        return 0;
+        return num;
     }
 }
