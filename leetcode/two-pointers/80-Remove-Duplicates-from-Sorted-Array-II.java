5433e2dea382e79b3c6980c1d66faf72e647e743
Kumar Yash Raj <kumaryashraj21@gmail.com>
2026-09-09 17:48:27 +0530

    Add Remove-Duplicates-from-Sorted-Array-II (two-pointers)

--- leetcode/two-pointers/80-Remove-Duplicates-from-Sorted-Array-II.java
+++ leetcode/two-pointers/80-Remove-Duplicates-from-Sorted-Array-II.java
@@ -1,14 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        HashMap<Integer,Integer>mpp = new HashMap<>();
+        // HashMap<Integer,Integer>mpp = new HashMap<>();
         int idx=0;
-        for(int i=0;i<nums.length;i++){
-            if(mpp.getOrDefault(nums[i],0)<2){
-                nums[idx]=nums[i];
-                idx++;
-                mpp.put(nums[i],mpp.getOrDefault(nums[i],0)+1);
-            }
+       for(int num:nums){
+        if(idx<2 || nums[idx-2]<num){
+            nums[idx]=num;
+            idx++;
         }
+       }
         return idx;
     }
 }
