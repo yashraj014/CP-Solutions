c5d3f1431cb9aa0f5bc6c046123d85e79c944943
Kumar Yash Raj <kumaryashraj21@gmail.com>
2026-09-09 16:48:53 +0530

    Add Group-Anagrams (hash-table)

---
+++ leetcode/hash-table/49-Group-Anagrams.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>>mpp = new HashMap<>();
+
+        for(String s:strs){
+            // String temp = s;
+            // Collections.sort(temp);
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+
+            String temp = new String(chars);
+
+            if(!mpp.containsKey(temp)){
+                mpp.put(temp,new ArrayList<>());
+            }
+
+            mpp.get(temp).add(s);
+        }
+        List<List<String>> ans = new ArrayList<>(mpp.values());
+       
+        return ans;
+    }
+}
